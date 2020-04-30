e2488028d2e7051f875ce19f573e85c4b77bda17
WalnutPeach <gostrenkom@gmail.com>
2020-04-30 15:02:57 +0300

    Add solved task11 lab01

---
+++ src/main/java/ua/edu/npu/lab01/task11/LengthSumLess.java
@@ -0,0 +1,26 @@
+package ua.edu.npu.lab01.task11;
+
+public class LengthSumLess {
+    Length l = new Length();
+    int i = 0;
+    double lSum = 0;
+    double lessSum;
+    double lengthSum (PointArr Arr) {
+        for (int j = 0; j < Arr.pArr.length; j++) {
+            lSum += l.findLength(Arr.pArr[i], Arr.pArr[j]);
+        }
+        i++;
+        return lSum;
+    }
+
+    public double lengthSumLess (PointArr Arr) {
+        lessSum = lengthSum(Arr);
+        for (int k = 1; k < Arr.pArr.length; k++) {
+            if (lessSum < lengthSum(Arr)) {
+                lessSum = lSum;
+            }
+            lSum = 0;
+        }
+        return lessSum;
+    }
+}
