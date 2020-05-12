6318c95bfa7a5e2137458577c4c00dba23a54b88
WalnutPeach <gostrenkom@gmail.com>
2020-05-12 18:53:30 +0300

    Fixed task0202

--- src/main/java/ua/edu/npu/lab02/task02/Application.java
+++ src/main/java/ua/edu/npu/lab02/task02/Application.java
@@ -2,9 +2,9 @@ package ua.edu.npu.lab02.task02;
 
 public class Application {
     public static void main(String[] args) {
-        Dog First = new Dog("Alex", 3);
-        Dog Second = new Dog("Mike", 5);
-        First.voice();
-        Second.voice();
+        Dog first = new Dog("Alex", 3);
+        Dog second = new Dog("Mike", 5);
+        first.voice();
+        second.voice();
     }
 }
